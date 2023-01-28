7f8faf034e0b540fc7558d0a0a3ffa603e6f71d9
AlexDrags <vera2052@mail.ru>
2023-01-28 19:44:56 +0300

    Доработка по кнопке воспоизведения

--- source/js/modules/player/init-player.js
+++ source/js/modules/player/init-player.js
@@ -1,16 +1,19 @@
 const mediaLayer = document.querySelector('.about__cover');
+const playMedia = document.querySelector('.about__player-wrapper img');
 const iframe = document.getElementById('iframe');
 
-// console.log(iframe);
+
 function removeLayer() {
   mediaLayer.style.display = 'none';
+  playMedia.style.display = 'none';
   iframe.setAttribute('src', 'https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1');
 }
 
-mediaLayer.addEventListener('click', removeLayer);
-mediaLayer.addEventListener('keyup', (event)=>{
+playMedia.addEventListener('click', removeLayer);
+playMedia.addEventListener('keyup', (event)=>{
   if (event.keyCode === 13) {
     mediaLayer.style.display = 'none';
+    playMedia.style.display = 'none';
     iframe.setAttribute('src', 'https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1');
   }
 });
