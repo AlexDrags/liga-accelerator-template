e996ed3fd8981ae9604184482d64e0563148afc5
AlexDrags <vera2052@mail.ru>
2023-01-28 19:00:15 +0300

    Правки после тестирования

--- source/js/modules/player/init-player.js
+++ source/js/modules/player/init-player.js
@@ -1,12 +1,16 @@
 const mediaLayer = document.querySelector('.about__cover');
+const iframe = document.getElementById('iframe');
 
+// console.log(iframe);
 function removeLayer() {
   mediaLayer.style.display = 'none';
+  iframe.setAttribute('src', 'https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1');
 }
 
 mediaLayer.addEventListener('click', removeLayer);
 mediaLayer.addEventListener('keyup', (event)=>{
   if (event.keyCode === 13) {
     mediaLayer.style.display = 'none';
+    iframe.setAttribute('src', 'https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1');
   }
 });
